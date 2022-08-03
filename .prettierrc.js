b2991c49cba57730e7991a413098bd10907f7185
moyongkexingo <moyongkexing@gmail.com>
2022-08-03 21:48:11 +0900

    prettierの設定ファイル追加

---
+++ .prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  tabWidth: 2,
+  printWidth: 100,
+  trailingComma: 'all', // 新しく行追加したい時に今の最終行の最後にコンマつけなくてよくなる
+  semi: true, // 議論はあるがPrettierのデフォルトがtrueなのでそれにしたがう
+  singleQuote: true, // Next.jsは基本的シングルクォーテーション(')を使うらしい
+};
