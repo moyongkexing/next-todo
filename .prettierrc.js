4a2412fb7ca207a920de4c6ae2f5831801996970
moyongkexingo <moyongkexing@gmail.com>
2022-08-04 19:23:53 +0900

    コメント修正

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,7 @@
 module.exports = {
   tabWidth: 2,
   printWidth: 100,
-  trailingComma: 'all', // 新しく行追加したい時に今の最終行の最後にコンマつけなくてよくなる
+  trailingComma: 'all', // 新しく行追加したい時に一つ前の行にコンマつけなくてよくなる
   semi: true, // 議論はあるがPrettierのデフォルトがtrueなのでそれにしたがう
   singleQuote: true, // Next.jsは基本的シングルクォーテーション(')を使うらしい
 };
